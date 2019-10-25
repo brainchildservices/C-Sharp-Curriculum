2005e9cdb20b6e578d7d455fc4b911e75544940e
Aneesh S <aneeshs005@gmail.com>
2019-10-25 17:16:47 +0530

    Added Test for Method overloding

---
+++ Chapter 1/Solution4.cs
@@ -0,0 +1,15 @@
+namespace Problem4
+{
+    public class Solution4
+    {
+        public double  Add(double a, double b, double c)
+        {
+            double Total=a+b+c;
+            return Total;
+        }
+        public double Add(double  a, double  b)
+        {
+            return a+b;
+        }
+    }
+}
